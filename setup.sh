eaca182720de143be3a8ea3d043c633d19c0bf78
DaanGrob05 <d.grob05@gmail.com>
2024-04-14 08:51:58 +0200

    setup script\

---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Update and install
+sudo apt update && sudo apt upgrade -y
+sudo apt install git-all curl gcc ffmpeg ubuntu-restricted-extras libavcodec-extra ripgrep fd-find fzf xclip bat tmux -y
+
+# Flatpak
+sudo apt install flatpak
+sudo apt install plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# # Manual action
+# git config --global user.name "UserName"
+# git config --global user.email "Email"
+# install neovim appimage
+
+# Github SSH keg
+ssh-keygen -t ed25519 -C "d.grob05@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub # Copy this output and add key in github
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+
+# Node and NPM
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
+
+# Set NPM Global path
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo 'export PATH=~/.npm-global/bin:$PATH' >>~/.zshrc
+
+# Bun
+curl -fsSL https://bun.sh/install | bash
+exec /usr/bin/zsh
+
+# LazyGit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+# Github CLI
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+	sudo apt update &&
+	sudo apt install gh -y#
+
+# install rose pine kde theme
+git clone git@github.com:ashbork/kde.git ~/rose-pine-kde/ # install settings > appearance > plasma style > install from file > select the .tar.gz file
+
+# Bruno CLI
+npm i -g @usebruno/cli#
+
+# install fish shell
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+chsh -s ${which fish}
+
+# Install Starship Prompt
+curl -sS https://starship.rs/install.sh | sh
+
+# Jagex Launcher and RuneLite
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists JagexLauncher https://jagexlauncher.flatpak.mcswain.dev/JagexLauncher.flatpakrepo
+# Required for 32-bit compatibility. Jagex Launcher is 32-bit.
+flatpak install --user flathub org.freedesktop.Platform.Compat.i386/x86_64/23.08
+flatpak install --user flathub org.freedesktop.Platform.GL32.default/x86_64/23.08
+# Install the launcher itself
+flatpak install --user JagexLauncher com.jagex.Launcher
+# Install RuneLite (if desired)
+flatpak install --user JagexLauncher com.jagex.Launcher.ThirdParty.RuneLite
