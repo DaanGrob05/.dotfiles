165c3f531474bd6856a3223eae362fb6e27c2d13
DaanGrob05 <d.grob05@gmail.com>
2024-05-30 12:28:08 +0200

    install kubectl

--- setup.sh
+++ setup.sh
@@ -86,3 +86,7 @@ flatpak install --user JagexLauncher com.jagex.Launcher.ThirdParty.RuneLite
 
 # Install zoxide
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
