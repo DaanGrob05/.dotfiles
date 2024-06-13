918a301455e9ce10753c92852bec759873a899da
DaanGrob05 <d.grob05@gmail.com>
2024-06-13 16:31:06 +0200

    add gh dash install

--- setup.sh
+++ setup.sh
@@ -57,6 +57,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
 	sudo apt update &&
 	sudo apt install gh -y#
+gh extension install dlvhdr/gh-dash
 
 # install rose pine kde theme
 git clone git@github.com:ashbork/kde.git ~/rose-pine-kde/ # install settings > appearance > plasma style > install from file > select the .tar.gz file
