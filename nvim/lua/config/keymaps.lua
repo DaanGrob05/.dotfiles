99481d95c93d82cfbd91cdcf3622f055bff26f3b
DaanGrob05 <d.grob05@gmail.com>
2024-09-26 11:09:25 +0200

    add neogen and inc-rename plugins with keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,5 +3,7 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>", { desc = "Disable Copilot" })
-vim.keymap.set("n", "<leader>cen", "<cmd>Copilot enable<cr>", { desc = "Enabre Copilot" })
+vim.keymap.set("n", "<leader>cen", "<cmd>Copilot enable<cr>", { desc = "Enable Copilot" })
 vim.keymap.set("n", "<leader>sar", "<cmd>GrugFar<cr>", { desc = "Open GrugFar" })
+vim.keymap.set("n", "<leader>ng", "<cmd>Neogen<cr>", { desc = "Neogen" })
+vim.keymap.set("n", "<leader>rn", ":IncRename ", { desc = "IncRename" })
