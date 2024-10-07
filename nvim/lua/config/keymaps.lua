0c9fee38c32abdb5a5176ae00263e49db0ac4c74
DaanGrob05 <d.grob05@gmail.com>
2024-10-07 12:21:24 +0200

    find files including hidden and ignored

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,4 +6,15 @@ vim.keymap.set("n", "<leader>cd", "<cmd>Copilot disable<cr>", { desc = "Disable 
 vim.keymap.set("n", "<leader>cen", "<cmd>Copilot enable<cr>", { desc = "Enable Copilot" })
 vim.keymap.set("n", "<leader>sar", "<cmd>GrugFar<cr>", { desc = "Open GrugFar" })
 vim.keymap.set("n", "<leader>ng", "<cmd>Neogen<cr>", { desc = "Neogen" })
-vim.keymap.set("n", "<leader>rn", ":IncRename ", { desc = "IncRename" })
+vim.keymap.set("n", "<leader>rn", ":IncRename", { desc = "IncRename" })
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", function()
+  builtin.find_files({
+    hidden = true, -- Enable hidden files
+    no_ignore = true, -- Disable .gitignore and other ignore files
+    no_ignore_parent = true, -- Disable ignoring in parent directories
+    file_ignore_patterns = { "node_modules/", "dist/", "git/" },
+  })
+end, { noremap = true, silent = true, desc = "Find files (including hidden, no ignore)" })
