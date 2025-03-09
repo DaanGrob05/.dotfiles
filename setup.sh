2fc063b1e1e51504ef8dc0142f962549ab1ffc25
DaanGrob05 <d.grob05@gmail.com>
2025-03-09 20:09:25 +0100

    clean up setup

--- setup.sh
+++ setup.sh
@@ -2,13 +2,12 @@
 
 # Update and install
 sudo apt update && sudo apt upgrade -y
-sudo apt install git-all curl gcc ffmpeg ubuntu-restricted-extras libavcodec-extra ripgrep fd-find fzf xclip bat tmux gnome-tweaks gnome-shell-extensions build-essential -y
+sudo apt install git-all curl gcc ffmpeg ubuntu-restricted-extras libavcodec-extra ripgrep fd-find fzf xclip bat tmux gnome-tweaks gnome-shell-extensions build-essential tmuxinator nodejs npm flatpak pipx -y
 
 # Set up caps lock to be ctrl and both shift to be caps lock
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps', 'shift:both_capslock', 'shift:both_capslock_cancel']"
 
 # Flatpak
-sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # # Manual action
@@ -49,10 +48,6 @@ cat ~/.ssh/id_ed25519.pub # Copy this output and add key in github
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 
-# Bun
-curl -fsSL https://bun.sh/install | bash
-exec /usr/bin/zsh
-
 # LazyGit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -60,7 +55,6 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
 # Github CLI
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
 	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
@@ -75,7 +69,7 @@ npm i -g @usebruno/cli
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
-chsh -s /usr/bin/fish
+chsh -s $(which fish)
 
 # Install Starship Prompt
 curl -sS https://starship.rs/install.sh | sh
@@ -141,25 +135,20 @@ sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "$HOME/.docker" -R
 
 # Install N
-curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | sudo bash -s lts
-# If you want n installed, you can use npm now.
-npm install -g n
-np lts
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
+# Source before continuing
+npm install -g n
+np lts
 sudo rm -rf /usr/local/bin/node
+sudo apt remove nodejs npm
 
 # Pipx
-sudo apt update
-sudo apt install pipx
 pipx ensurepath
 sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
 
 # Posting
 pipx install posting
-
-# Install tmuxinator
-sudo apt install tmuxinator
 
 # Install asdf, erlang and elixir
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
