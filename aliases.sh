04ca9b97b043ef7e443e1d79200bdccc47d941c9
DaanGrob05 <d.grob05@gmail.com>
2025-03-09 19:47:33 +0100

    clean up aliases

--- aliases.sh
+++ aliases.sh
@@ -2,10 +2,6 @@ alias update="sudo apt update && sudo apt upgrade -y && flatpak update"
 alias cleanup="sudo apt autoremove && sudo apt autoclean"
 alias uac="update && cleanup"
 
-alias nordvpn-pause="nordvpn d && sleep 300 && nordvpn c netherlands"
-alias nordC="nordvpn c netherlands"
-alias nordD="nordvpn d"
-
 alias docker-stop='docker ps -aq | xargs docker stop'
 
 alias a="artisan"
@@ -22,32 +18,18 @@ alias lad="lazydocker"
 
 alias nvimconfig="cd ~/.config/nvim && vim . && cd -"
 
-alias cw="cd ~/coding/codewars/"
-alias lc="cd ~/coding/leetcode/"
-alias cod="cd ~/coding/"
-
 alias ghcreateinit="git add . && git commit -m \"Commit before first push to Github\" && gh repo create --private --push --source ."
 alias ghcreate="gh repo create --private --push --source ."
 
 alias fman="compgen -c | fzf | xargs -r fman"
 alias fd="fdfind"
-# alias cdall="cd $(fdfind -t d | fzf)"
 
 alias bat="batcat"
 
-alias sp="cd ~/BigBridge/SmartPeak/"
-
-alias dote="cd ~/.dotfiles && vim . && cd -"
-alias dotu="cd ~/.dotfiles && git pull"
-
-alias c="z" # easier to type than z and is close to cd
-
-alias doco="docker-compose"
+alias dote="cd ~/.dotfiles && vim && cd -"
 
 alias sail="./vendor/bin/sail"
 alias a="artisan"
-
-alias gsw="git switch"
 
 alias m="bin/magento"
 
