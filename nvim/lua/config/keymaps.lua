b13a72d3b44c443b3791afd784aa10fad264a062
DaanGrob05 <d.grob05@gmail.com>
2025-10-06 12:20:01 +0200

    update lazyvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,31 +19,33 @@ end, { noremap = true, silent = true, desc = "Grep (including hidden, no ignore)
 Snacks.toggle.zen():map("<leader>z")
 
 -- Nvim Dap keybinds to mimic Jetbrains debugger
-local dap = require("dap")
-local dapui = require("dapui")
--- Keybindings for debugging (mimicking PhpStorm)
-vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" }) -- Start/Continue
-vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" }) -- Toggle breakpoint
-vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" }) -- Step over
-vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" }) -- Step into
-vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" }) -- Step out
 
--- Breakpoint management
-vim.keymap.set("n", "<Shift-F9>", function()
-  dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-end, { desc = "Debug: Set Conditional Breakpoint" }) -- Conditional breakpoint
+-- local dap = require("dap")
+-- local dapui = require("dapui")
 
-vim.keymap.set("n", "<Ctrl-F9>", function()
-  dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-end, { desc = "Debug: Set Logpoint" }) -- Log point
+-- -- Keybindings for debugging (mimicking PhpStorm)
+-- vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" }) -- Start/Continue
+-- vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" }) -- Toggle breakpoint
+-- vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" }) -- Step over
+-- vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" }) -- Step into
+-- vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" }) -- Step out
 
--- DAP UI
-vim.keymap.set("n", "<Ctrl-F8>", dapui.toggle, { desc = "Debug: Toggle Debug UI" }) -- Open/Close debug UI
-
--- Stopping debugging
-vim.keymap.set("n", "<Shift-F5>", dap.terminate, { desc = "Debug: Stop" }) -- Stop debugging
-
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "{", "{zz")
-vim.keymap.set("n", "}", "}zz")
+-- -- Breakpoint management
+-- vim.keymap.set("n", "<Shift-F9>", function()
+--   dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+-- end, { desc = "Debug: Set Conditional Breakpoint" }) -- Conditional breakpoint
+--
+-- vim.keymap.set("n", "<Ctrl-F9>", function()
+--   dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+-- end, { desc = "Debug: Set Logpoint" }) -- Log point
+--
+-- -- DAP UI
+-- vim.keymap.set("n", "<Ctrl-F8>", dapui.toggle, { desc = "Debug: Toggle Debug UI" }) -- Open/Close debug UI
+--
+-- -- Stopping debugging
+-- vim.keymap.set("n", "<Shift-F5>", dap.terminate, { desc = "Debug: Stop" }) -- Stop debugging
+--
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "{", "{zz")
+-- vim.keymap.set("n", "}", "}zz")
