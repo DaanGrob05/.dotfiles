380735725e211342b317bea414972481f08518c2
DaanGrob05 <d.grob05@gmail.com>
2026-01-28 14:44:33 +0100

    add sql auto format

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,28 +1,44 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    formatters_by_ft = {
-      html = { "prettier" },
-      json = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      lua = { "stylua"}
-    },
-    format_after_save = function(bufnr)
-      local disable = vim.b[bufnr].disable_autoformat or vim.g.disable_autoformat
-      if disable then
-        return
-      end
-      return {
-        lsp_fallback = false,
-        async = true,
-      }
-    end,
-  },
-  config = function(_, opts)
-    require("conform").setup(opts)
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatters_by_ft = {
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			lua = { "stylua" },
+			sql = { "pg_format" },
+		},
+
+		formatters = {
+			pg_format = {
+				command = "pg_format",
+				args = {
+					"--spaces",
+					"2",
+					"--wrap-limit",
+					"0",
+					"--keyword-case",
+					"2",
+				},
+				stdin = true,
+			},
+		},
+		format_after_save = function(bufnr)
+			local disable = vim.b[bufnr].disable_autoformat or vim.g.disable_autoformat
+			if disable then
+				return
+			end
+			return {
+				lsp_fallback = false,
+				async = true,
+			}
+		end,
+	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+	end,
 }
