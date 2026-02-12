1bcb5f80e22f95ee4a45d96e924ceadbe99892d0
DaanGrob05 <d.grob05@gmail.com>
2026-02-12 16:32:55 +0100

    add php installs. correct composer path

--- config.fish
+++ config.fish
@@ -8,16 +8,15 @@ end
 
 source ~/.dotfiles/aliases.sh
 source ~/.dotfiles/fish_functions/index.fish
-# source ~/.asdf/asdf.fish
 
 set PATH /home/daan/.npm-global/bin/ $PATH
 set PATH /home/daan/.cargo/bin $PATH
 set PATH /home/daan/.local/bin $PATH
 set PATH /usr/local/go/bin $PATH
 set PATH /home/daan/go/bin $PATH
-set PATH /.config/composer/vendor/bin $PATH
 set PATH /home/daan/.local/share/bob/nvim-bin/ $PATH
 set PATH /home/daan/bin $PATH
+set PATH /home/daan/.config/composer/vendor/bin $PATH
 
 starship init fish | source
 
