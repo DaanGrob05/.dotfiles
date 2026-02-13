f70b403b4de6dfe06ba3d92e847750c529397125
DaanGrob05 <d.grob05@gmail.com>
2026-02-13 15:53:19 +0100

    remove old laravel fish functions. Add fish completion code

--- fish_functions/index.fish
+++ fish_functions/index.fish
@@ -1,4 +1,3 @@
-source ~/.dotfiles/fish_functions/laravel.fish
 source ~/.dotfiles/fish_functions/take.fish
 source ~/.dotfiles/fish_functions/btinfo.fish
 source ~/.dotfiles/fish_functions/go_mod_init.fish
--- setup_scripts/php.sh
+++ setup_scripts/php.sh
@@ -4,5 +4,9 @@ sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php # Press enter to confirm.
 sudo apt update
 
 sudo apt install php8.4-cli php8.4-fpm
-sudo apt install php8.4-common php8.4-{bcmath,bz2,curl,gd,gmp,intl,mbstring,opcache,readline,xml,zip}
-sudo apt install php8.4-xdebug
+sudo apt install php8.4-common php8.4-{bcmath,bz2,curl,gd,gmp,intl,mbstring,opcache,readline,xml,zip,sqlite3,xdebug}
+
+# Install completion files for artisan
+curl -L --create-dirs -o ~/.config/fish/completions/artisan.fish https://github.com/adriaanzon/fish-artisan-completion/raw/master/completions/artisan.fish
+curl -L --create-dirs -o ~/.config/fish/completions/php.fish https://github.com/adriaanzon/fish-artisan-completion/raw/master/completions/php.fish
+curl -L --create-dirs -o ~/.config/fish/functions/artisan.fish https://github.com/adriaanzon/fish-artisan-completion/raw/master/functions/artisan.fish
