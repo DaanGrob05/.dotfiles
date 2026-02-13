e1cd80ae259bdc682083fbe97543b2739c23c2a9
DaanGrob05 <d.grob05@gmail.com>
2026-02-13 15:14:16 +0100

    correct ubuntu php install script

--- setup_scripts/apt_install.sh
+++ setup_scripts/apt_install.sh
@@ -2,4 +2,4 @@
 
 # Update and install
 sudo apt update && sudo apt upgrade -y
-sudo apt install git-all curl gcc ffmpeg ubuntu-restricted-extras libavcodec-extra ripgrep fd-find fzf xclip bat tmux build-essential tmuxinator nodejs npm flatpak pipx protobuf-compiler python3 python3-tk python3-venv php php-cli php-mbstring php-pgsql php-mysql libapache2-mod-php -y
+sudo apt install git-all curl gcc ffmpeg ubuntu-restricted-extras libavcodec-extra ripgrep fd-find fzf xclip bat tmux build-essential tmuxinator nodejs npm flatpak pipx protobuf-compiler python3 python3-tk python3-venv -y
